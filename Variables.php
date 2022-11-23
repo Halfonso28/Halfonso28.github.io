1dafe535a56f99f8eeb265232163a828a6a6bd05
Halfonso28 <lael28sword@gmail.com>
2022-11-22 20:53:30 -0600

    ProgLab

--- Variables.php
+++ Variables.php
@@ -14,9 +14,16 @@ $ORN=$_POST['ORN'];
 
 $ECOLI="A/A+-++--+-+";
 $ECOLIINACTIVA="A/A+-++----+";
+$GRUPOSABC="Alc/A--+-/+-----";//b
+$GRUPOSABC1="Alc/A--++-----";//c
+$GRUPOSABC2="Alc/A--+------";//d
 $SSONNEI="Alc/A--+-----+";
-$SBOYDII="Alc/A--+------";
-$ETARDA="Alc/A++++--+++";
+$SBOYDII="Alc/A--+------";//d
+$SFLEXNERI="Alc/A--+-/+-----";//b
+$SFLEXNERI1="Alc/A--++-----";//c
+$SFLEXNERI2="Alc/A--+------";//d
+$EDWARSIELLA="Alc/A++++--+++";//a
+$ETARDA="Alc/A++++--+++";//a
 $HOSHINAE="Alc/A+++---+++";
 $SALMONELLA="Alc/A+++-+-+++";
 $STYPHI="Alc/A+++---+--";
@@ -55,6 +62,7 @@ $PALCALIFACIENS="Alc/A+/--+++-+--";
 $PALCALIFACIENS1="Alc/A+-+++-+--";
 $PALCALIFACIENS2="Alc/A--+++-+--";
 $YENTEROCOLITICA="";//5
+$YPESTIS="Alc/A--+------";//d
 $INTERMEDIA="Alc/A--++-+---";
 
 
